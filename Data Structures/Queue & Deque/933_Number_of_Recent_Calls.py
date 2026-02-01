000700c36e4699a999c7b3a9128e15a2ccc0bbf4
Jou Yi Lee <s1083816@berkeley.edu>
2026-01-31 21:06:38 -0800

    feat: Add solution for LC226_Invert_Binary_Tree (Use Recursive and change the node value)

---
+++ Data Structures/Queue & Deque/933_Number_of_Recent_Calls.py
@@ -0,0 +1,16 @@
+class RecentCounter(object):
+
+    def __init__(self):
+        self.q = deque()
+        
+
+    def ping(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        self.q.append(t)
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+        
+        return len(self.q)

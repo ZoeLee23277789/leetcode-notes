73e7201e8756d11efdd6568e8ba387a390ddbc25
Jou Yi Lee <s1083816@berkeley.edu>
2026-02-03 18:39:01 -0800

    feat: Add solution for LC733_Flood_Fill (DFS(Hard))

---
+++ Algorithms/BFS/733_Flood_Fill.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        row = len(image)
+        col = len(image[0])
+
+        old = image[sr][sc]
+        if old == color:
+            return image
+
+        def dfs(r,c):
+            if r < 0 or r >= row or c < 0 or c >= col:
+                return 
+            
+            if image[r][c] != old:
+                return
+
+            image[r][c] = color
+
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+        dfs(sr,sc)
+        return image

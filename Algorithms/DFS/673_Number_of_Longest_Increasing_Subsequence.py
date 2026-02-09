f15cbb0be31dd0cdce2efbdf1d72050cb2c4889d
Jou Yi Lee <s1083816@berkeley.edu>
2026-02-08 18:36:40 -0800

    feat: Add solution for LC673_Number_of_Longest_Increasing_Subsequence (  1. 最大長度（max） 2.達到最大長度的路徑數量（count）)

---
+++ Algorithms/DFS/673_Number_of_Longest_Increasing_Subsequence.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findNumberOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # 這題需要
+        # 最大長度（max）
+        # 達到最大長度的路徑數量（count）
+        if len(nums) == 0:
+            return 0
+
+        length = [1] * len(nums)
+        count = [1] * len(nums)
+        ans = 0
+
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if length[j] + 1 > length[i]:
+                        length[i] = length[j] + 1
+                        count[i] = count[j]
+                    elif length[j] + 1 == length[i]:
+                        count[i] += count[j]
+
+        L = max(length)
+
+        for i in range(len(nums)):
+            if length[i] == L:
+                ans += count[i]
+        return ans
